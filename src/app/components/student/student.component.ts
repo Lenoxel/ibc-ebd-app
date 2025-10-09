a1d632581cfce6d72ee272663253dd89f97872b5
gabriel.lenon <glenonsilva@gmail.com>
2025-10-09 00:54:52 -0300

    feat: :sparkles: add options to remove student and toggle student ebd relationship
    no api linked yet

--- src/app/components/student/student.component.ts
+++ src/app/components/student/student.component.ts
@@ -1,18 +1,29 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { ActionSheetController, AlertController } from '@ionic/angular';
 import { IStudent } from 'src/app/interfaces';
 
 @Component({
   selector: 'app-student',
   templateUrl: './student.component.html',
   styleUrls: ['./student.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StudentComponent implements OnInit {
   @Input() student: IStudent;
   @Input() show = true;
   @Output() selectStudentEvent = new EventEmitter<IStudent>();
 
-  constructor() { }
+  constructor(
+    private readonly actionSheetCtrl: ActionSheetController,
+    private readonly alertController: AlertController
+  ) {}
 
   ngOnInit() {}
 
@@ -40,4 +51,64 @@ export class StudentComponent implements OnInit {
     window.location.href = `tel:819${formattedNumber}`;
   }
 
+  async openStudentOptions() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: `Tornar ${
+            this.student.ebd_relation === 'visitante' ? 'Aluno' : 'Visitante'
+          }`,
+          data: { action: 'toggleRole' },
+          handler: () => {
+            this.toggleRelation();
+          },
+        },
+        {
+          text: 'Remover da Classe',
+          role: 'destructive',
+          data: { action: 'delete' },
+          handler: () => {
+            this.removeFromClass();
+          },
+        },
+        {
+          text: 'Voltar',
+          role: 'cancel',
+          data: { action: 'cancel' },
+          handler: () => {
+            console.log('Ação cancelada');
+          },
+        },
+      ],
+    });
+
+    await actionSheet.present();
+  }
+
+  async toggleRelation() {
+    // await this.studentService.toggleRelation(this.student.id);
+
+    this.student.ebd_relation =
+      this.student.ebd_relation === 'visitante' ? 'aluno' : 'visitante';
+
+    const alert = await this.alertController.create({
+      header: 'Sucesso',
+      message: `${this.student.name} agora é um(a) ${this.student.ebd_relation}.`,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
+  async removeFromClass() {
+    // await this.studentService.remove(this.student.id);
+
+    const alert = await this.alertController.create({
+      header: 'Sucesso',
+      message: `${this.student.name} foi removido(a) da classe.`,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
 }
--- src/app/interfaces/student.ts
+++ src/app/interfaces/student.ts
@@ -1,15 +1,16 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 export interface IStudent {
-    id: number;
-    name: string;
-    picture: string;
-    ebd_class: string;
-    date_of_birth?: string;
-    is_birthday_person: boolean;
-    whatsapp?: string;
-    work_on_sundays?: boolean;
-    frequency?: {
-        absences_in_sequence: number;
-        presences_in_sequence: number;
-    };
+  id: number;
+  name: string;
+  picture: string;
+  ebd_class: string;
+  date_of_birth?: string;
+  is_birthday_person: boolean;
+  whatsapp?: string;
+  work_on_sundays?: boolean;
+  frequency?: {
+    absences_in_sequence: number;
+    presences_in_sequence: number;
+  };
+  ebd_relation?: string;
 }
